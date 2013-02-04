a03f4daf01749551507ce4098ce92003352eb936
Yao-Cheng Li <dreamworkshe@gmail.com>
2013-02-03 18:56:06 -0800

    Add more word filters (http, rt, original keyword)

--- app/controllers/getter_controller.rb
+++ app/controllers/getter_controller.rb
@@ -3,9 +3,10 @@ require 'twitter'
 class GetterController < ApplicationController
   @@stop_words = ['a','able','about','across','after','all','almost','also','am','among','an','and','any','are','as','at','be','because','been','but','by','can','cannot','could','dear','did','do','does','either','else','ever','every','for','from','get','got','had','has','have','he','her','hers','him','his','how','however','i','if','in','into','is','it','its','just','least','let','like','likely','may','me','might','most','must','my','neither','no','nor','not','of','off','often','on','only','or','other','our','own','rather','said','say','says','she','should','since','so','some','than','that','the','their','them','then','there','these','they','this','tis','to','too','twas','us','wants','was','we','were','what','when','where','which','while','who','whom','why','will','with','would','yet','you','your']
   def get
-    word = params[:word]
+    word = params[:word].downcase
     freq = Hash.new()
-    Twitter.search(word, :count => 200, :result_type => "recent", :lang => "en").results.map do |status|
+    Twitter.search(word, :count => 250, :result_type => "recent", :lang => "en").results.map do |status|
+      #puts status.text
       tokens = status.text.downcase.split(/\W+/)
       tokens.each do |t|
         if freq.has_key?(t)
@@ -15,6 +16,23 @@ class GetterController < ApplicationController
         end
       end
     end
+
+    # delete http
+    if freq.has_key? 'http'
+      freq.delete('http')
+    end
+
+    # delete search word
+    if freq.has_key? word
+      freq.delete(word)
+    end
+
+    # delete retweet
+    if freq.has_key? 'rt'
+      freq.delete('rt')
+    end
+
+    # delete stopwords
     @@stop_words.each do |sw|
       if freq.has_key? sw
         freq.delete(sw)
