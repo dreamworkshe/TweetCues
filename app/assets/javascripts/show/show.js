70038b28f41cbde7516e7e1de7a7cee5f9690433
Yao-Cheng Li <dreamworkshe@gmail.com>
2013-02-26 18:09:20 -0800

    Add simple drag&drop, add click search

--- app/assets/javascripts/show/show.js
+++ app/assets/javascripts/show/show.js
@@ -1,20 +1,43 @@
 function initShow() {
+  var spinner;
+  var request, running = false;
+
   var search = function() {
+    if (running) {
+      request.abort();
+    }
+    running = true;
+    //$("#wordcloud").attr("disabled", "disabled");
     $("#wordcloud").empty();
     var word = $("#search_word").val();
-    $.getJSON("/get", {word: word}, function(data) {
+    request = $.getJSON("/get", {word: word}, function(data) {
       var freq = [];
       // $.each(data, function(index, pair) {
       //   freq.push({text: pair.word, weight: pair.count});
       // });
       for (var i = 0; i < data.length && i < 100; i++) {
-        freq.push({text: data[i].word, weight: data[i].count});
+        freq.push({ text: data[i].word, weight: data[i].count,
+          html: { draggable: "true" }, handlers: { click:clickWord } });
       }
-      $("#wordcloud").jQCloud(freq, {width: 700, height: 400});
+      $("#wordcloud").jQCloud(freq,
+        { width: 700, height: 400,
+          afterCloudRender: function() { running = false; } });
     });
   };
 
+  var clickWord = function() {
+    if (running) {
+      return;
+    }
+    var new_search_word = $(this).text();
+    new_search_word = new_search_word.substring(0,1) +
+      new_search_word.substring(1,new_search_word.length).toLowerCase();
+    $("#search_word").val(new_search_word);
+    search();
+  }
+
   $("#search_button").click(search);
+
   $("#search_word").keypress(function(event) {
     if (event.which == 13) {
       search();
@@ -22,4 +45,22 @@ function initShow() {
   });
 
 
+};
+
+
+// jQuery plugin for spin.js
+$.fn.spin = function(opts) {
+  this.each(function() {
+    var $this = $(this),
+        data = $this.data();
+
+    if (data.spinner) {
+      data.spinner.stop();
+      delete data.spinner;
+    }
+    if (opts !== false) {
+      data.spinner = new Spinner($.extend({color: $this.css('color')}, opts)).spin(this);
+    }
+  });
+  return this;
 };
